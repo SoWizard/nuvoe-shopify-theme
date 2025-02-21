6aa1514b6a454fe0f862543af3cec9711ea7800f
SoWizard <SoWizard@users.noreply.github.com>
2025-02-21 12:46:18 +1100

    Add Microsoft Clarity tracking

---
+++ assets/custom.js
@@ -0,0 +1,10 @@
+/* 
+* Custom Javascript file for the Nuvoe theme
+*/ 
+
+/* Microsoft Clarity tracking script */
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "qd2h04rtvj");
